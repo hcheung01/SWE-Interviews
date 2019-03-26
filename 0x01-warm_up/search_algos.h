336f02d8cc972ea359db1f8fbc21b4006fe11588
hcheung01 <hcheung01@gmail.com>
2019-03-26 00:53:29 +0000

    updated header guarded info

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef _SEARCH_ALGOS_H_
-#define _SEARCH _ALGOS_H_
+#define _SEARCH_ALGOS_H_
 
 int binary_search(int *array, size_t size, int value);
 void printer(int *array, int i, int size);
+
 #endif
